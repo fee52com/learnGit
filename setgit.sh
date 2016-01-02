e24778413de5c7606fb5fdc1b4eea84f44d7b972
fee52com <fee52com@gmail.com>
2016-01-02 21:12:05 +0800

    add script

---
+++ setgit.sh
@@ -0,0 +1,9 @@
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
+git config --global user.name fee52com
+git config --global user.email fee52com@gmail.com
